e597d9028d6585a32a55a8d91149ad2573c2885c
Leo <82315240+leohonlmc@users.noreply.github.com>
2023-10-19 23:46:23 -0400

    home page update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./partial/Header";
 import Footer from "./partial/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faImages,
+  faUserLargeSlash,
+  faEarthAmericas,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   const [timeLeft, setTimeLeft] = useState({
@@ -46,7 +52,7 @@ function Home() {
         </div>
         <div className="countdown">
           <p>{`December 25 ${new Date().getFullYear()} ⏳`}</p>
-          <div className="countdown-time">
+          <div className="countdown-time days">
             <div>{timeLeft.days}:</div>
             <div>days</div>
           </div>
@@ -67,6 +73,111 @@ function Home() {
         <div className="start-btn">
           <button className="btn btn-danger">Get Started</button>
           <button className="btn btn-outline-light">What is Réjouir?</button>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className="mission-div">
+          <div className="mission">
+            <FontAwesomeIcon
+              icon={faImages}
+              size="2xl"
+              style={{ color: "#dc3545" }}
+            />
+            <p style={{ margin: "0px", marginTop: "10px" }}>
+              <strong>Christmas Blog</strong>{" "}
+            </p>
+            <p
+              style={{
+                width: "70%",
+                textAlign: "center",
+                margin: "auto",
+                marginTop: "10px",
+              }}
+            >
+              Share up to <strong>5 images</strong> about Christmas from your
+              country!
+            </p>
+          </div>
+          <div className="mission">
+            <FontAwesomeIcon
+              icon={faEarthAmericas}
+              size="2xl"
+              style={{ color: "#dc3545" }}
+            />
+            <p style={{ margin: "0px", marginTop: "10px" }}>
+              <strong>Connect the Globe</strong>{" "}
+            </p>
+            <p
+              style={{
+                width: "70%",
+                textAlign: "center",
+                margin: "auto",
+                marginTop: "10px",
+              }}
+            >
+              We <strong>CAPTURE</strong> the Christmas spirit from around the
+              globe.
+            </p>
+          </div>
+          <div className="mission">
+            <FontAwesomeIcon
+              icon={faUserLargeSlash}
+              size="2xl"
+              style={{ color: "#dc3545" }}
+            />
+            <p style={{ margin: "0px", marginTop: "10px" }}>
+              <strong>Guest Visit</strong>{" "}
+            </p>
+            <p
+              style={{
+                width: "70%",
+                textAlign: "center",
+                margin: "auto",
+                marginTop: "10px",
+              }}
+            >
+              Don't need an account to share your Christmas moment!
+            </p>
+          </div>
+        </div>
+
+        <div className="d-flex">
+          <div className="feature-img">
+            <img src="/moment-2.png" alt="" style={{ width: "100%" }} />
+          </div>
+          <div className="feature right">
+            <div className="feature-child ">
+              <h2>
+                <strong>SHARE YOUR MOMENT 🎇</strong>
+              </h2>
+              <hr className="feature-hr" />
+              <p>
+                Share festive moments from around the globe in our Christmas
+                blog without an account.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <br />
+
+        <div className="d-flex">
+          <div className="feature-img">
+            <img src="/christmas-tree.png" alt="" style={{ width: "100%" }} />
+          </div>
+          <div className="feature">
+            <div className="feature-child">
+              <h2>
+                <strong>WE ❤️ CHRISTMAS, THAT's IT.</strong>
+              </h2>
+              <hr className="feature-hr" />
+              <p>
+                Come back every year to feel the Christmas spirit and share your
+                Christmas moment with us. 🌟
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
--- src/components/partial/Footer.jsx
+++ src/components/partial/Footer.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useRef } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { useNavigate } from "react-router-dom";
@@ -8,16 +7,15 @@ function Footer() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <footer className="container-footer container text-center my-6 p-8 text-gray3">
-        <div className="mx-auto w-24 h-1 my-12 bg-gradient-to-r from-gray5 to-gray4 rounded-full"></div>
+    <footer className="container-footer container text-center my-6 p-8 text-gray3">
+      <div className="mx-auto w-24 h-1 my-12 bg-gradient-to-r from-gray5 to-gray4 rounded-full"></div>
 
-        <div className="py-3">
-          <img src="/R.png" alt="" style={{ width: "130px" }} />
-          {/* <p className="footer-brand-name">Réjouir.com</p> */}
-        </div>
+      <div className="py-3">
+        <img src="/R.png" alt="" style={{ width: "130px" }} />
+        {/* <p className="footer-brand-name">Réjouir.com</p> */}
+      </div>
 
-        {/* <a
+      {/* <a
           href="https://www.instagram.com/theswebuilder/"
           className="instagram"
           target="_blank"
@@ -32,7 +30,7 @@ function Footer() {
 
         <br />
         <br /> */}
-        {/* 
+      {/* 
         <a href="/about" className="footer-intro-link">
           About
         </a>
@@ -44,7 +42,7 @@ function Footer() {
         <br />
         <br /> */}
 
-        {/* <div className="py-3 footer-normal">
+      {/* <div className="py-3 footer-normal">
           <a href="/terms" className="contact-me-nav">
             Terms & Conditions
           </a>
@@ -56,11 +54,13 @@ function Footer() {
           <br />
         </div> */}
 
-        <div className="text-xs" style={{ fontSize: "0.8rem" }}>
-          Copyright © 2023 <strong>Réjouir.com</strong> <br />
-        </div>
-      </footer>
-    </div>
+      <div
+        className="text-xs"
+        style={{ fontSize: "0.8rem", color: "white", fontWeight: "bold" }}
+      >
+        Copyright © 2023 <strong>Réjouir.com</strong> <br />
+      </div>
+    </footer>
   );
 }
 
