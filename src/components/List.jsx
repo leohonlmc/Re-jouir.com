d795832096d022001b1d9ff41ab9640d6db76283
Leo <82315240+leohonlmc@users.noreply.github.com>
2024-01-07 21:10:53 -0500

    Preview fixed top

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -750,6 +750,18 @@ function List() {
                           fontWeight: "bold",
                           marginBottom: "10px",
                         }}
+                        onClick={() =>
+                          toast.warn("Please upload at least one image!", {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                          })
+                        }
                       >
                         Preview
                       </div>
