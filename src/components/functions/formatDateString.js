c5867573b95dd25ef1cb0be202f0c3c62d3857e9
Leo <82315240+leohonlmc@users.noreply.github.com>
2024-02-01 12:11:22 -0500

    momnet package added to replace the formatdatestring logic

--- src/components/functions/formatDateString.js
+++ src/components/functions/formatDateString.js
@@ -1,26 +1,8 @@
 import React from "react";
+import moment from "moment";
 
 function formatDateString(dateString) {
-  const currentDateTime = new Date();
-  const targetDateTime = new Date(dateString);
-  const timeDifference = currentDateTime - targetDateTime;
-
-  if (timeDifference < 60000) {
-    const seconds = Math.floor(timeDifference / 1000);
-    return `${seconds} seconds ago`;
-  } else if (timeDifference < 3600000) {
-    const minutes = Math.floor(timeDifference / 60000);
-    return `${minutes} minutes ago`;
-  } else if (timeDifference < 86400000) {
-    const hours = Math.floor(timeDifference / 3600000);
-    return `${hours} hours ago`;
-  } else if (timeDifference < 2592000000) {
-    const days = Math.floor(timeDifference / 86400000);
-    return `${days} days ago`;
-  } else {
-    const months = Math.floor(timeDifference / 2592000000);
-    return `${months} months ago`;
-  }
+  return moment(dateString).fromNow();
 }
 
 export default formatDateString;
