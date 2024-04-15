1bc32a5ec836320fb29ed426542411e95e1807c6
Leo <82315240+leohonlmc@users.noreply.github.com>
2024-04-15 17:25:06 -0400

    Style changed

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -377,7 +377,7 @@ function List() {
   };
 
   return (
-    <div className="List" style={{ paddingTop: "100px" }}>
+    <div className="List">
       <div className="list-header-section">
         <Header showSearchBar={false} title={`Upload | Réjouir`} />
       </div>
